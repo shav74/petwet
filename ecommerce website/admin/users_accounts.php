123faf72830a385c75efec9b910c37d6c96c5dfe
shavinda <arunshavinda@icloud.com>
2023-08-12 16:22:22 +0530

    ss

--- ecommerce website/admin/users_accounts.php
+++ ecommerce website/admin/users_accounts.php
@@ -6,11 +6,11 @@ session_start();
 
 $admin_id = $_SESSION['admin_id'];
 
-if(!isset($admin_id)){
+if (!isset($admin_id)) {
    header('location:admin_login.php');
 }
 
-if(isset($_GET['delete'])){
+if (isset($_GET['delete'])) {
    $delete_id = $_GET['delete'];
    $delete_user = $conn->prepare("DELETE FROM `users` WHERE id = ?");
    $delete_user->execute([$delete_id]);
@@ -29,6 +29,7 @@ if(isset($_GET['delete'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -40,38 +41,47 @@ if(isset($_GET['delete'])){
    <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
+
 <body>
 
-<?php include '../components/admin_header.php'; ?>
+   <?php include '../components/admin_header.php'; ?>
 
-<section class="accounts">
+   <section class="accounts">
 
-   <h1 class="heading">user accounts</h1>
+      <h1 class="heading">Customers</h1>
 
-   <div class="box-container">
+      <div class="box-container">
 
-   <?php
-      $select_accounts = $conn->prepare("SELECT * FROM `users`");
-      $select_accounts->execute();
-      if($select_accounts->rowCount() > 0){
-         while($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)){   
-   ?>
-   <div class="box">
-      <p> user id : <span><?= $fetch_accounts['id']; ?></span> </p>
-      <p> username : <span><?= $fetch_accounts['name']; ?></span> </p>
-      <p> email : <span><?= $fetch_accounts['email']; ?></span> </p>
-      <a href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>" onclick="return confirm('delete this account? the user related information will also be delete!')" class="delete-btn">delete</a>
-   </div>
-   <?php
+         <?php
+         $select_accounts = $conn->prepare("SELECT * FROM `users`");
+         $select_accounts->execute();
+         if ($select_accounts->rowCount() > 0) {
+            while ($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)) {
+               ?>
+               <div class="box">
+                  <p> user id : <span>
+                        <?= $fetch_accounts['id']; ?>
+                     </span> </p>
+                  <p> username : <span>
+                        <?= $fetch_accounts['name']; ?>
+                     </span> </p>
+                  <p> email : <span>
+                        <?= $fetch_accounts['email']; ?>
+                     </span> </p>
+                  <a href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>"
+                     onclick="return confirm('delete this account? the user related information will also be delete!')"
+                     class="delete-btn">delete</a>
+               </div>
+               <?php
+            }
+         } else {
+            echo '<p class="empty">no accounts available!</p>';
          }
-      }else{
-         echo '<p class="empty">no accounts available!</p>';
-      }
-   ?>
+         ?>
 
-   </div>
+      </div>
 
-</section>
+   </section>
 
 
 
@@ -84,7 +94,8 @@ if(isset($_GET['delete'])){
 
 
 
-<script src="../js/admin_script.js"></script>
-   
+   <script src="../js/admin_script.js"></script>
+
 </body>
+
 </html>
